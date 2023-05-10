cdee3834f29618a69eca4e88afa0d99cec20aa11
ZhengJin <fangzhengjin@gmail.com>
2023-05-10 15:49:38 +0800

    fix: 分片处理修正

--- src/main/kotlin/me/zhengjin/common/attachment/controller/vo/MultipartUploadCreateRequestVO.kt
+++ src/main/kotlin/me/zhengjin/common/attachment/controller/vo/MultipartUploadCreateRequestVO.kt
@@ -1,9 +1,5 @@
 package me.zhengjin.common.attachment.controller.vo
 
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
-import java.util.Locale
-
 class MultipartUploadCreateRequestVO {
     // 文件名称
     var fileName: String? = null
