32554b4e00888f575c86c2dbabbc27f042bfd66e
ZhengJin <fangzhengjin@gmail.com>
2023-05-10 16:21:04 +0800

    fix: 分片处理修正

--- src/main/kotlin/me/zhengjin/common/attachment/controller/vo/MultipartUploadCreateRequestVO.kt
+++ src/main/kotlin/me/zhengjin/common/attachment/controller/vo/MultipartUploadCreateRequestVO.kt
@@ -1,8 +1,25 @@
 package me.zhengjin.common.attachment.controller.vo
 
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+
 class MultipartUploadCreateRequestVO {
     // 文件名称
+    @NotEmpty(message = "文件名称不能为空")
     var fileName: String? = null
+
     // 分片数量
+    @NotNull(message = "分片数量不能为空")
     var chunkSize: Int? = null
+
+    // 其他信息
+    @NotEmpty(message = "业务模块不能为空")
+    var module: String? = null
+
+    @NotEmpty(message = "业务类型不能为空")
+    var businessTypeCode: String? = null
+
+    @NotEmpty(message = "业务类型名称不能为空")
+    var businessTypeName: String? = null
+    var pkId: String? = null
 }
